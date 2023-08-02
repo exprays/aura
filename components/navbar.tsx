09622a46aad21579b8f3e8cf799781f7e6ff74aa
Surya <suryakant.subudhi@outlook.com>
2023-08-02 19:48:25 +0530

    feat: created navbar component

---
+++ components/navbar.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { Menu, Sparkles } from "lucide-react";
+import Link from "next/link";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import { ModeToggle } from "./theme-toggle";
+
+const font = Poppins({
+    weight: "600",
+    subsets: ["latin"]
+})
+
+export const Navbar = () => {
+    return (
+        <div
+            className="
+                fixed
+                w-full
+                flex
+                justify-between
+                z-50
+                items-center
+                py-2
+                px-4
+                border-b
+                border-primary/10
+                bg-secondary
+            "
+        >
+            <div className="flex items-center">
+                <Menu className="block md:hidden"/>
+                <Link href="/">
+                    <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary",
+                        font.className
+                    )}>
+                        aura.ai
+                    </h1>
+                </Link>
+            </div>
+            <div
+                className="
+                    flex
+                    items-center
+                    gap-x-3
+                "
+            >
+                <Button variant="candy" size="sm">
+                    Upgrade
+                    <Sparkles 
+                        className="
+                            h-4
+                            w-4
+                            fill-white
+                            text-white
+                            ml-2
+                        "
+                    />
+                </Button>
+                <ModeToggle />
+                <UserButton />
+            </div>
+        </div>
+    )
+}
