fc608062dcc4b9945db068b67b098a2f6a2f3efe
Surya <suryakant.subudhi@outlook.com>
2023-08-03 12:12:35 +0530

    📱feat: added categories feature

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,10 +1,16 @@
+import { Categories } from "@/components/categories";
 import { SearchInput } from "@/components/searchinput";
+import prismadb from "@/lib/prismadb";
 
 
-const Rootpage = () => {
+const Rootpage = async () => {
+
+    const categories = await prismadb.category.findMany();
+
     return ( 
         <div className="h-full p-4 space-y-2">
             <SearchInput />
+            <Categories data={categories}/>
         </div>
      );
 }
