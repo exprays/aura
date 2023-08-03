ea9ba567dc93555231bc43a3290e7d1d11061d7a
Surya <suryakant.subudhi@outlook.com>
2023-08-03 16:16:36 +0530

    🪶Aura creation from completed

--- app/(root)/(routes)/aura/[auraId]/interface/auraform.tsx
+++ app/(root)/(routes)/aura/[auraId]/interface/auraform.tsx
@@ -5,9 +5,31 @@ import { Aura, Category } from "@prisma/client";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Separator } from "@/components/ui/separator";
 import { ImageUpload } from "@/components/imageupload";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Wand2 } from "lucide-react";
+
+
+const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
+`;
+
+const SEED_CHAT = `Human: Hi Elon, how's your day been?
+Elon: Busy as always. Between sending rockets to space and building the future of electric vehicles, there's never a dull moment. How about you?
+
+Human: Just a regular day for me. How's the progress with Mars colonization?
+Elon: We're making strides! Our goal is to make life multi-planetary. Mars is the next logical step. The challenges are immense, but the potential is even greater.
+
+Human: That sounds incredibly ambitious. Are electric vehicles part of this big picture?
+Elon: Absolutely! Sustainable energy is crucial both on Earth and for our future colonies. Electric vehicles, like those from Tesla, are just the beginning. We're not just changing the way we drive; we're changing the way we live.
+
+Human: It's fascinating to see your vision unfold. Any new projects or innovations you're excited about?
+Elon: Always! But right now, I'm particularly excited about Neuralink. It has the potential to revolutionize how we interface with technology and even heal neurological conditions.
+`;
 
 
 interface AuraFormProps {
@@ -102,6 +124,160 @@ export const AuraForm = ({
                             </FormItem>
                         )}
                     />
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <FormField 
+                            name="name"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem className="col-span-2 md:col-span-1">
+                                    <FormLabel>
+                                        Name
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input 
+                                            disabled={isLoading}
+                                            placeholder="Elon Musk"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormDescription>
+                                        This is how your aura will be named
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField 
+                            name="description"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem className="col-span-2 md:col-span-1">
+                                    <FormLabel>
+                                        Description
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input 
+                                            disabled={isLoading}
+                                            placeholder="CEO & Founder of Tesla & Space X"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormDescription>
+                                        Describe your aura
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField 
+                            name="categoryId"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Category</FormLabel>
+                                    <Select
+                                        disabled={isLoading}
+                                        onValueChange={field.onChange}
+                                        value={field.value}
+                                        defaultValue={field.value}
+                                    >
+                                        <FormControl>
+                                            <SelectTrigger className="bg-background">
+                                                <SelectValue 
+                                                    defaultValue={field.value}
+                                                    placeholder="Select a category"
+                                                />
+                                            </SelectTrigger>
+                                        </FormControl>
+                                        <SelectContent>
+                                            {categories.map((category) => (
+                                                <SelectItem
+                                                    key={category.id}
+                                                    value={category.id}
+                                                >
+                                                    {category.name}
+                                                </SelectItem>
+                                            ))}
+                                        </SelectContent>
+                                    </Select>
+                                    <FormDescription>
+                                        Select a category
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <div className="space-y-2 w-full">
+                        <div>
+                            <h3 className="text-lg font-medium bg-gradient-to-r from-pink-600 via-purple-500 to-rose-500 bg-clip-text text-transparent animate-text">
+                                Configuration
+                            </h3>
+                            <p className="text-sm text-muted-foreground">
+                                Detailed instructions for your aura AI behaviour
+                            </p>
+                        </div>
+                        <Separator className="bg-primary/10"/>
+                    </div>
+                    <FormField 
+                            name="instructions"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem className="col-span-2 md:col-span-1">
+                                    <FormLabel>
+                                        Instructions
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Textarea
+                                            className="bg-background resize-none"
+                                            rows={7} 
+                                            disabled={isLoading}
+                                            placeholder={PREAMBLE}
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormDescription>
+                                        Describe your Aura&apos;s backstory & relevant details in detail! 
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField 
+                            name="seed"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem className="col-span-2 md:col-span-1">
+                                    <FormLabel>
+                                        Example Conversation
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Textarea
+                                            className="bg-background resize-none"
+                                            rows={7} 
+                                            disabled={isLoading}
+                                            placeholder={SEED_CHAT}
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormDescription>
+                                        Describe your Aura&apos;s behaviour on user Interaction!
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <div className="w-full flex justify-center">
+                                <Button
+                                    size="lg"
+                                    disabled={isLoading}
+                                    className="hover:scale-110"
+                                    variant="rose"
+                                >
+                                    {initialData ? "Edit Your Aura": "Create your Aura"}
+                                    <Wand2 className="w-4 h-4 ml-2"/>
+                                </Button>
+                        </div>
                 </form>
             </Form>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -65,10 +65,23 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        "text": {
+          '0%, 100%': {
+             'background-size':'200% 200%',
+              'background-position': 'left center'
+          },
+          '50%': {
+             'background-size':'200% 200%',
+              'background-position': 'right center'
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        'animation': {
+          'text':'text 2s ease infinite',
+        },
       },
     },
   },
