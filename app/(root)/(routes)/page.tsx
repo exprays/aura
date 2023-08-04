5ca61730ac752eb32478b8fa50f1acdc8fbf85bd
Surya <suryakant.subudhi@outlook.com>
2023-08-04 12:01:21 +0530

    📱Added elements to show auras

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,37 @@
+import { Auras } from "@/components/auras";
 import { Categories } from "@/components/categories";
 import { SearchInput } from "@/components/searchinput";
 import prismadb from "@/lib/prismadb";
 
+interface RootPageProps {
+    searchParams: {
+        categoryId: string;
+        name: string;
+    }
+}
 
-const Rootpage = async () => {
+const Rootpage = async ({
+    searchParams
+}: RootPageProps) => {
+
+    const data = await prismadb.aura.findMany({
+        where: {
+            categoryId: searchParams.categoryId,
+            name: {
+                search: searchParams.name
+            }
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+        include: {
+            _count: {
+                select: {
+                    messages: true
+                }
+            }
+        }
+    })
 
     const categories = await prismadb.category.findMany();
 
@@ -11,6 +39,7 @@ const Rootpage = async () => {
         <div className="h-full p-4 space-y-2">
             <SearchInput />
             <Categories data={categories}/>
+            <Auras data={data} />
         </div>
      );
 }
