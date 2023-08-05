11ae84db40e9bd75a37c1056197bd80362b0385a
Surya <suryakant.subudhi@outlook.com>
2023-08-05 10:53:30 +0530

    ⚙️vars to handle AI interaction

--- app/(chat)/(routes)/chat/[chatId]/interface/chatclient.tsx
+++ app/(chat)/(routes)/chat/[chatId]/interface/chatclient.tsx
@@ -2,6 +2,9 @@
 
 import { ChatHeader } from "@/components/chatheader";
 import { Aura, Message } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+import { useCompletion } from "ai/react";
 
 interface ChatClientProps {
     aura: Aura & {
@@ -15,9 +18,49 @@ interface ChatClientProps {
 export const ChatClient = ({
     aura
 }: ChatClientProps) => {
+
+	const router = useRouter();
+	const [messages, setMessages] = useState<any[]>(aura.messages);
+
+	const {
+		input,
+		isLoading,
+		handleInputChange,
+		handleSubmit,
+		setInput,
+	} = useCompletion({
+		api: `/api/chat/${aura.id}`,  // waiting for the response of api which will generate response of AI model
+		onFinish(prompt, completion) {
+			const systemMessage = {   // Store the response
+				role: "system",
+				content: completion,
+			}
+
+			setMessages((current) => [...current, systemMessage])
+			setInput("");
+
+			router.refresh();
+		}
+	});
+
+	const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+		const userMessage = {        // Storing the user response
+			role: "user",
+			content: input,
+		};
+
+		setMessages((current) => [...current, userMessage]);
+
+		handleSubmit(e);
+	}
+
+
     return (
         <div className="flex flex-col h-full p-4 space-y-2">
             <ChatHeader aura={aura}/>
+			<div>
+				Messages
+			</div>
         </div>
     )
 }
